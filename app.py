ac1144b6bafcb0ebffe8e94849640450b51c03ec
NimrodIsaac <Isaackavyuu@gmail.com>
2025-03-04 20:48:57 +0300

    Removed HTML file and updated app.py

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return "Nimrod Maingi for President 2027!"
 
 if __name__ == '__main__':
     app.run(debug=True)
